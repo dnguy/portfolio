82eab01a67b59deb78d72d152c24f70c7e2d2cd2
dnguy <dnguy008@gmail.com>
2015-05-31 21:16:59 -0700

    updated layout

--- assets/main.js
+++ assets/main.js
@@ -1,29 +1,29 @@
-function loadpage(page){
-	$.ajax({
-		url:'assets/pages/'+page+'.html',
-		dataType:'html',
-		cache: false,
-		success: function(response){
-			$('.main_content').html(response);
-		}
-	})
-}
+// function loadpage(page){
+// 	$.ajax({
+// 		url:'assets/pages/'+page+'.html',
+// 		dataType:'html',
+// 		cache: false,
+// 		success: function(response){
+// 			$('.main_content').html(response);
+// 		}
+// 	})
+// }
 
-$(document).ready(function(){
-	$('.home_link').click(function(){
-		loadpage('home');
-	})
-	$('.about_link').click(function(){
-		loadpage('about');
-	})
-		$('.skills_link').click(function(){
-		loadpage('skills');
-	})
-	$('.contact_link').click(function(){
-		loadpage('contact');
-	})
+// $(document).ready(function(){
+// 	$('.home_link').click(function(){
+// 		loadpage('home');
+// 	})
+// 	$('.about_link').click(function(){
+// 		loadpage('about');
+// 	})
+// 		$('.skills_link').click(function(){
+// 		loadpage('skills');
+// 	})
+// 	$('.contact_link').click(function(){
+// 		loadpage('contact');
+// 	})
 
-});
+// });
 
 $(document).on('click','.navbar-collapse.in',function(e) {
     if( $(e.target).is('a') ) {
